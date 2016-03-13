92f6e86ff9af855df46448f909f41a978a0a7e69
Emmanuel Florent <emmanuel.florent@gmail.com>
2016-03-13 10:05:07 +0000

    Setup Gradle Build/Tests on Android Studio

--- assets/database/create/currency.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists currency (
-	_id integer primary key autoincrement,
-	name text not null,
-	title text not null,
-	symbol text not null
-);
-	
--- src/com/flowzr/utils/EntityEnum.java
+++
@@ -1,7 +0,0 @@
-package com.flowzr.utils;
-
-public interface EntityEnum extends LocalizableEnum {
-	
-	int getIconId();
-
-}
